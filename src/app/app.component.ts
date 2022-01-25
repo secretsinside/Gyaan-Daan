0475844587466b4d9008cf3330ada25512b199f0
secretsinside <07nith.me@gmail.com>
2022-01-26 00:29:50 +0530

    notificatio POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import * as Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+import $ from 'jquery';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +13,29 @@ export class AppComponent {
   // userAuthenticated: boolean;
   title = 'Gyaan-Daan';
 
+  url = 'http://localhost:9090/notifications/';
+  client: any;
+  greeting: any;
+
   constructor() {
     // this.userAuthenticated = false;
+    this.connection();
+  }
+
+  connection(){
+    let ws = new SockJS(this.url);
+    this.client = Stomp.over(ws);
+    let that = this;
+	
+    this.client.connect({}, function(frame: any) {
+      that.client.subscribe("/queue/volunteer/sagarika.nangia@gmail.com/notification", (message: any) => {
+        console.log(message);
+        if(message.body) {
+          that.greeting = message.body;
+		  //$(".msg").append(this.greeting)
+		  $(".msg").html(that.greeting)
+        }
+      });
+    });
   }
 }
