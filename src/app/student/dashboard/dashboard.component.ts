d0e0a4bf58e646906ee8611ec9956559bb828790
secretsinside <07nith.me@gmail.com>
2022-01-27 01:54:24 +0530

    feature | added pending and accepted request screens

--- src/app/student/dashboard/dashboard.component.ts
+++ src/app/student/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,14 +16,31 @@ export class DashboardComponent implements OnInit {
   notificationType: string;
   notificationMessage: string;
 
-  constructor() {
+  seePendingRequest: boolean;
+  acceptedRequest: any;
+  hideNotification: boolean;
+  pendingRequest: any;
+
+  constructor(private studentService: StudentService) {
     this.seeRequestStatus = false;
     this.notification = false;
     this.notificationType = "";
     this.notificationMessage = "";
+    this.hideNotification = false;
+    this.seePendingRequest = false;
   }
 
   ngOnInit(): void {
+    if(this.userDetail.userType === 'volunteer'){
+      this.studentService.getAcceptedRequest(this.userDetail.email).subscribe(
+        (data: any) => {
+          this.acceptedRequest = data.responseInfo;
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      )
+    }
   }
 
   raiseNotification(event: any): void {
@@ -34,6 +52,23 @@ export class DashboardComponent implements OnInit {
   closeNotification(): void {
     this.notification = false;
     this.notificationMessage = "";
+  }
+
+  openPendingRequestWindow(): void {
+    this.hideNotification = true;
+    this.studentService.getEligibleRequest(this.userDetail.email).subscribe(
+      (data: any) => {
+        this.pendingRequest = data.responseInfo;
+        this.seePendingRequest = true;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
+  }
+
+  openAcceptedRequestWindow(): void {
+    this.seePendingRequest = false;
   }
 
 }
